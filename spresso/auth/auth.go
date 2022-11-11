09de77ee19dd6d114996dc59ccf6b3a3ae77cd0b
David <fumonchu@gmail.com>
2022-11-11 13:14:05 -0500

    minor tweak

--- spresso/auth/auth.go
+++ spresso/auth/auth.go
@@ -1,13 +1,39 @@
 package auth
 
+import (
+	"log"
+	"spresso-sdk-go/spresso/http_client"
+	"time"
+)
+
 type Auth struct {
 	ClientId     string
 	ClientSecret string
 	Audience     string "https://spresso-api"
 	GrantType    string "client_credentials"
 	Token        string "Bearer"
+	TTL          int
+	CreatedAt    time.Time "Time"
 }
 
-type PriceOptimizationRequest struct {
-	Data *PriceOptimizationRequestData `json:"data"`
+var (
+	auth = newAuth()
+)
+
+func NewAuth(clientId string, clientSecret string, logger log.Logger, restyClient http_client.RestyClient) {
+
+}
+
+func RenewToken() bool {
+	return true
+}
+
+func getToken() string {
+	return Auth.Token
+
+	return nil
+}
+
+func Authenticate() {
+
 }
